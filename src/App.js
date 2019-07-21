ced53d3f76908a378509942ad8f1c2d964a8ef29
Ruth Newman <ruthnewman@gmail.com>
2019-07-21 23:34:28 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <h1>Country Music Lyrics</h1>
+        <Switch>
+          <Route exact path="/" component={Index} />
+        </Switch>
+      </Fragment>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="nav-bar" >
+      <span></span>
+    </nav>
+  );
+}
+
+export default Navbar;
